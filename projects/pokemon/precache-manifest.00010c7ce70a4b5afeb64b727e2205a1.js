c99b669c53a459cb3fcd2b7760dcdfe90560b917
Simeon Kauffmann <simeon.kauffmann@gmail.com>
2021-02-01 14:28:17 +0100

    editing service worker

--- projects/pokemon/precache-manifest.00010c7ce70a4b5afeb64b727e2205a1.js
+++ projects/pokemon/precache-manifest.00010c7ce70a4b5afeb64b727e2205a1.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "52a7fed5cd66a2f99efc",
-    "url": "/css/app.2bd75c8d.css"
+    "url": "projects/pokemon/css/app.2bd75c8d.css"
   },
   {
     "revision": "c4ea34a4647c507a68a20c63e4142954",
-    "url": "/index.html"
+    "url": "projects/pokemon/index.html"
   },
   {
     "revision": "52a7fed5cd66a2f99efc",
-    "url": "/js/app.91aa7a17.js"
+    "url": "projects/pokemon/js/app.91aa7a17.js"
   },
   {
     "revision": "562f381f868e438b9763",
-    "url": "/js/chunk-vendors.dcc5cff0.js"
+    "url": "projects/pokemon/js/chunk-vendors.dcc5cff0.js"
   },
   {
     "revision": "37ff510fec96aac74a7799e176528b19",
-    "url": "/manifest.json"
+    "url": "projects/pokemon/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "projects/pokemon/robots.txt"
   }
 ]);
