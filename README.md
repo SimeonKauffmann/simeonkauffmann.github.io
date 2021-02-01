0292b10515155e9097d8d78cdac6f0716e18a18e
Simeon Kauffmann <simeon.kauffmann@gmail.com>
2021-02-01 18:43:36 +0100

    reverting back to original solution

--- README.md
+++
@@ -1,3 +0,0 @@
-# Simeon Kauffmann
-
-This is my personal profile that can be seen at <https://simeonkauffmann.github.io/>
---
+++ precache-manifest.00010c7ce70a4b5afeb64b727e2205a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a7fed5cd66a2f99efc",
+    "url": "projects/pokemon/css/app.2bd75c8d.css"
+  },
+  {
+    "revision": "c4ea34a4647c507a68a20c63e4142954",
+    "url": "projects/pokemon/index.html"
+  },
+  {
+    "revision": "52a7fed5cd66a2f99efc",
+    "url": "projects/pokemon/js/app.91aa7a17.js"
+  },
+  {
+    "revision": "562f381f868e438b9763",
+    "url": "projects/pokemon/js/chunk-vendors.dcc5cff0.js"
+  },
+  {
+    "revision": "37ff510fec96aac74a7799e176528b19",
+    "url": "projects/pokemon/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "projects/pokemon/robots.txt"
+  }
+]);
